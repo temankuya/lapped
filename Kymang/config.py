8293728c7e402494d0e092e435071455a620fe8b
fkinfred <karissamaliaput13@gmail.com>
2024-06-14 06:24:45 +0800

    Update config.py

--- Kymang/config.py
+++ Kymang/config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "4793ebd12a1d545c668aa5e3aac11011")
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://fredbot:fredbot@cluster0.oahrt9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 ADMINS = [int(x) for x in (os.environ.get("ADMINS", "21037639").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "160").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-1002184614954"))
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002103101233"))
 BOT_ID = int(os.environ.get("BOT_ID", "7205564649"))
 
 KITA = [int(x) for x in (os.environ.get("KITA", "21037639").split())]
