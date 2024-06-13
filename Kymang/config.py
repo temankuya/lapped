3dcd8752a0927b64903c0dd486f27139a6d86279
fkinfred <karissamaliaput13@gmail.com>
2024-06-14 06:13:39 +0800

    Update config.py

--- Kymang/config.py
+++ Kymang/config.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "17131033"))
-API_HASH = os.environ.get("API_HASH", "7768488c115ac09684bb38e608c47997")
-MONGO_URL = os.environ.get("MONGO_URL", "")
-ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7205564649:AAEAfz-BktDjIpm4xD5P8SjrtkRzIfoP4qw")
+API_ID = int(os.environ.get("API_ID", "21037639"))
+API_HASH = os.environ.get("API_HASH", "4793ebd12a1d545c668aa5e3aac11011")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://fredbot:fredbot@cluster0.oahrt9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ADMINS = [int(x) for x in (os.environ.get("ADMINS", "21037639").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "160").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-100"))
-BOT_ID = int(os.environ.get("BOT_ID", ""))
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002198822633"))
+BOT_ID = int(os.environ.get("BOT_ID", "7205564649"))
 
-KITA = [int(x) for x in (os.environ.get("KITA", "1668766845").split())]
+KITA = [int(x) for x in (os.environ.get("KITA", "21037639").split())]
