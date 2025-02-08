4bdb8b058a4e97b729a0d006fb1e16ff1b0922d5
arthur130999 <165489760+arthur130999@users.noreply.github.com>
2025-02-08 14:21:49 +0700

    fix

--- Kymang/config.py
+++ Kymang/config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "4793ebd12a1d545c668aa5e3aac11011")
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://muhandrekurniawan:OUjBw8SpKs7hS8Vg@cluster0.q4rqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1755535456").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "160").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-1002086067332"))
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002403171925"))
 BOT_ID = int(os.environ.get("BOT_ID", "7413079332"))
 
 KITA = [int(x) for x in (os.environ.get("KITA", "1755535456").split())]
